f320a1f3a2d13bd15cd21e67da892d536b60e462
Jacobo Fiaño Rodriguez <72132311+jacobo-fiano1@users.noreply.github.com>
2023-06-12 21:04:58 +0200

    Update protectoraService.py

--- backAPM/protectora/protectoraService.py
+++ backAPM/protectora/protectoraService.py
@@ -170,7 +170,7 @@ class AnimalService:
         animal = Animal.objects.get(pk=idAnimal)
         animal.estado = estado
         animal.save()
-        return {"msg": "OK: Animal " + idAnimal + " registrado como : AD"}
+        return {"msg": "OK: Animal " + idAnimal + " registrado como : " + estado}
 
 
         
